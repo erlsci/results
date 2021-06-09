db70b43ec182d2311d80be634de393989354aa20
Duncan McGreggor <oubiwann@gmail.com>
2021-06-08 20:38:48 -0500

    Added more code for results handling.

--- src/results.erl
+++ src/results.erl
@@ -1,9 +1,16 @@
 -module(results).
 
--export([new/1, new/2]).
--export([value/1, error/1]).
+-compile({no_auto_import, [error/1]}).
+
+-export([new/0, new/1, new/2]).
+-export([value/1, values/1, error/1, errors/1]).
+-export([has_error/1, has_errors/1]).
 
 -include_lib("include/results.hrl").
+
+-spec new() -> result().
+new() ->
+    #result{}.
 
 -spec new(term()) -> result().
 new(Value) ->
@@ -19,9 +26,27 @@ new(Value, Error) ->
   }.
 
 -spec value(result()) -> term().
-value(Result) ->
-  Result#result.value.
+value(#result{value = V}) ->
+  V.
+
+-spec values([result()]) -> [term()].
+values(Results) ->
+  [value(R) || R <- Results].
 
 -spec error(result()) -> term().
-error(Result) ->
-  Result#result.error.
+error(#result{error = E}) ->
+  E.
+
+-spec errors([result()]) -> [term()].
+errors(Results) ->
+  [error(R) || R <- Results].
+
+-spec has_error(result()) -> boolean().
+has_error(#result{error = E}) when E =:= undefined ->
+    false;
+has_error(_) ->
+    true.
+
+-spec has_errors([result()]) -> [boolean()].
+has_errors(Results) ->
+    [has_error(R) || R <- Results].
--- test/results_tests.erl
+++ test/results_tests.erl
@@ -3,6 +3,10 @@
 -include_lib("eunit/include/eunit.hrl").
 -include_lib("include/results.hrl").
 
+new_empty_test() ->
+    R = results:new(),
+    ?assertEqual(undefined, R#result.value).
+
 new_result_value_test() ->
     R = results:new(alice),
     ?assertEqual(alice, R#result.value),
@@ -17,8 +21,44 @@ new_result_error_test() ->
 value_test() ->
     ?assertEqual(alice, results:value(results:new(alice))).
 
+values_test() ->
+    Rs = [results:new(alice),
+          results:new(bob),
+          results:new(carol)],
+    ?assertEqual([alice,bob,carol], results:values(Rs)).
+
 error_test() ->
     R1 = results:new(bob, {error, "AWOL again, Bob"}),
     ?assertEqual({error, "AWOL again, Bob"}, results:error(R1)),
     R2 = results:new(undefined, oops),
     ?assertEqual(oops, results:error(R2)).
+
+errors_test() ->
+    Rs = [results:new(undefined, {error, absent_alice}),
+          results:new(undefined, {error, bad_news_bob}),
+          results:new(undefined, {error, collups_cutting_carol})],
+    ?assertEqual([absent_alice,
+                  bad_news_bob,
+                  collups_cutting_carol],
+                 [element(2, R) || R <- results:errors(Rs)]).
+
+has_error_false_test() ->
+    ?assertNot(results:has_error(results:new())),
+    ?assertNot(results:has_error(results:new(alice))),
+    ?assertNot(results:has_error(results:new(alice, undefined))).
+
+has_error_true_test() ->
+    ?assert(results:has_error(results:new(nil, oops))),
+    ?assert(results:has_error(results:new(nil, {error, oops}))).
+
+has_errors_false_test() ->
+    Rs = [results:new(alice),
+          results:new(bob),
+          results:new(carol)],
+    ?assertEqual([false,false,false], results:has_errors(Rs)).
+
+has_errors_true_test() ->
+    Rs = [results:new(undefined, {error, absent_alice}),
+          results:new(undefined, {error, bad_news_bob}),
+          results:new(undefined, {error, collups_cutting_carol})],
+    ?assertEqual([true,true,true], results:has_errors(Rs)).
